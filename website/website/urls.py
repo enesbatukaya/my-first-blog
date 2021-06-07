15ef97627df3fb6c9c4fd788081bfc9a103fbd4c
enes <enesbatukaya@gmail.com>
2021-06-07 18:51:35 +0300

    post_list view olusturuldu

--- website/website/urls.py
+++ website/website/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('blog.urls'))
+    # anasayfaya gelen herkesi
+    # blog klasorundeki urls.py ye gonder
+    # ordaki yonergelere bak
 ]
